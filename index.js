f0903f3f17bffa5865b7ca238e1a5c34e3d3ce33
Phil Wekullo <philwekullo@gmail.com>
2023-12-12 02:58:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 9
+let num4 = 3
+const multiply = (num1 * num2)
+const random = Math.ceil(Math.random())
+const mod = (num3 % 5)
+const max = Math.max(20, num4, num2, num3)
